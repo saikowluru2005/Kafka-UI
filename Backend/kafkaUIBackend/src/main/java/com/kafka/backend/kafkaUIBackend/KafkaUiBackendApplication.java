40760ea72fb97444fe5f4c723f6bbde3a1595629
saikowluru2005 <saikowluru3@gmail.com>
2026-03-23 20:17:10 +0530

    Backend skeleton

---
+++ Backend/kafkaUIBackend/src/main/java/com/kafka/backend/kafkaUIBackend/KafkaUiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kafka.backend.kafkaUIBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaUiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaUiBackendApplication.class, args);
+	}
+
+}
